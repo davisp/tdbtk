6c7dee91d168f93b0007509c67a60e093cfd54ac
Paul J. Davis <paul.davis@tiledb.com>
2023-11-18 13:31:00 -0600

    Initial GenericTileIO deserialization implementation

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -4,7 +4,9 @@ pub mod compression;
 mod compressors;
 
 #[repr(u8)]
+#[derive(Clone, Copy, Debug, Default)]
 pub enum FilterType {
+    #[default]
     None = 0,
     GZip = 1,
     Zstd = 2,
@@ -12,7 +14,7 @@ pub enum FilterType {
     Rle = 4,
     BZip2 = 5,
     DoubleDelta = 6,
-    BitWdithReduction = 7,
+    BitWidthReduction = 7,
     BitShuffle = 8,
     ByteShuffle = 9,
     PositiveDelta = 10,
@@ -25,6 +27,35 @@ pub enum FilterType {
     Deprecated = 17,
     WebP = 18,
     Delta = 19,
+    InvalidFilterType = 255,
+}
+
+impl From<u8> for FilterType {
+    fn from(orig: u8) -> Self {
+        match orig {
+            0 => FilterType::None,
+            1 => FilterType::GZip,
+            2 => FilterType::Zstd,
+            3 => FilterType::LZ4,
+            4 => FilterType::Rle,
+            5 => FilterType::BZip2,
+            6 => FilterType::DoubleDelta,
+            7 => FilterType::BitWidthReduction,
+            8 => FilterType::BitShuffle,
+            9 => FilterType::ByteShuffle,
+            10 => FilterType::PositiveDelta,
+            11 => FilterType::Encryption,
+            12 => FilterType::ChecksumMD5,
+            13 => FilterType::ChecksumSHA256,
+            14 => FilterType::Dictionary,
+            15 => FilterType::ScaleFloat,
+            16 => FilterType::Xor,
+            17 => FilterType::Deprecated,
+            18 => FilterType::WebP,
+            19 => FilterType::Delta,
+            _ => return FilterType::InvalidFilterType,
+        }
+    }
 }
 
 pub trait Filter {
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -2,27 +2,69 @@ use binrw::binrw;
 
 use crate::filters::FilterType;
 
+fn is_compression_filter(ftype: FilterType) -> bool {
+    matches!(
+        ftype as FilterType,
+        FilterType::GZip
+            | FilterType::Zstd
+            | FilterType::Rle
+            | FilterType::BZip2
+            | FilterType::Delta
+            | FilterType::DoubleDelta
+            | FilterType::Dictionary
+    )
+}
+
+fn is_bit_width_reduction_filter(ftype: FilterType) -> bool {
+    matches!(ftype, FilterType::BitWidthReduction)
+}
+
+fn is_positive_delta_filter(ftype: FilterType) -> bool {
+    matches!(ftype, FilterType::PositiveDelta)
+}
+
+fn is_scale_float_filter(ftype: FilterType) -> bool {
+    matches!(ftype, FilterType::ScaleFloat)
+}
+
+fn is_webp_filter(ftype: FilterType) -> bool {
+    matches!(ftype, FilterType::WebP)
+}
+
+fn is_no_config_filter(ftype: FilterType) -> bool {
+    !(is_compression_filter(ftype)
+        || is_bit_width_reduction_filter(ftype)
+        || is_positive_delta_filter(ftype)
+        || is_scale_float_filter(ftype)
+        || is_webp_filter(ftype))
+}
+
+#[derive(Debug, Default)]
 #[binrw]
+#[brw(little)]
+#[br(import { filter_type: FilterType })]
 enum FilterConfig {
-    None,
+    #[br(pre_assert(is_compression_filter(filter_type)))]
     Compression {
-        compressor_type: u8,
+        #[br(map = |ftype: u8| ftype.into())]
+        #[bw(map = |ftype: &FilterType| *ftype as u8)]
+        compressor_type: FilterType,
         compression_level: i32,
-        #[br(if(compressor_type == FilterType::Delta as u8
-            || compressor_type == FilterType::DoubleDelta as u8))]
+        #[br(if(matches!(compressor_type,
+            FilterType::Delta | FilterType::DoubleDelta)))]
         reinterpret_type: u8,
     },
-    BitWidthReduction {
-        max_window_size: u32,
-    },
-    PositiveDelta {
-        max_window_size: u32,
-    },
-    FloatScaling {
+    #[br(pre_assert(is_bit_width_reduction_filter(filter_type)))]
+    BitWidthReduction { max_window_size: u32 },
+    #[br(pre_assert(is_positive_delta_filter(filter_type)))]
+    PositiveDelta { max_window_size: u32 },
+    #[br(pre_assert(is_scale_float_filter(filter_type)))]
+    ScaleFloat {
         scale: f64,
         offset: f64,
         byte_width: u64,
     },
+    #[br(pre_assert(is_webp_filter(filter_type)))]
     WebP {
         quality: f32,
         format: u8,
@@ -30,18 +72,33 @@ enum FilterConfig {
         y_extent: u16,
         x_extent: u16,
     },
+    #[default]
+    #[br(pre_assert(is_no_config_filter(filter_type)))]
+    None,
 }
 
+#[derive(Debug, Default)]
 #[binrw]
-struct FilterHeader {
-    filter_type: u8,
+#[brw(little)]
+struct Filter {
+    #[br(map = |ftype: u8| ftype.into())]
+    #[bw(map = |ftype: &FilterType| *ftype as u8)]
+    filter_type: FilterType,
     metadata_len: u32,
+
+    #[br(args { filter_type })]
+    config: FilterConfig,
 }
 
+#[derive(Debug)]
 #[binrw]
+#[brw(little)]
 struct FilterPipeline {
     max_chunk_size: u32,
     num_filters: u32,
+
+    #[br(count(num_filters))]
+    filters: Vec<Filter>,
 }
 
 #[derive(Debug)]
@@ -54,7 +111,7 @@ struct GenericTileHeader {
     datatype: u8,
     cell_size: u64,
     encryption_type: u8,
-    filter_pipeline_size: u64,
+    filter_pipeline_size: u32,
 }
 
 #[cfg(test)]
@@ -70,5 +127,8 @@ mod tests {
         let mut reader = Cursor::new(data);
         let header = GenericTileHeader::read(&mut reader).unwrap();
         println!("{:?}", header);
+
+        let pipeline = FilterPipeline::read(&mut reader).unwrap();
+        println!("{:?}", pipeline);
     }
 }
